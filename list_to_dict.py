d147894f1c82e76e453e90adec7c235af2baf212
kushalkumarkain <kushalkumar.kain156@gmail.com>
2022-07-19 10:35:20 +0530

    added code file with trial code

---
+++ list_to_dict.py
@@ -0,0 +1,17 @@
+# lst = [ ] 
+# n = int(input("Enter number of elements : ")) 
+  
+# for i in range(0, n): 
+#     ele = [input(), int(input())] 
+#     lst.append(ele) 
+      
+# print(lst)
+
+Lst = [] 
+maxLengthList = int(input("NO of inputs"))
+while len(Lst) < maxLengthList:
+    item = input("Enter your Item to the List: ")
+    Lst.append(item)
+    print(Lst)
+print("That's your Shopping List")
+print(Lst)
